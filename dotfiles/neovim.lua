9bfd344b09eb83720aabd33604a97e74a0410bd1
David Cornu <me@davidcornu.com>
2023-04-01 15:34:31 -0600

    use the default preset for cmp

--- dotfiles/neovim.lua
+++ dotfiles/neovim.lua
@@ -113,9 +113,9 @@ cmp.setup({
     { name = "buffer" },
     { name = "path" }
   }),
-  mapping = {
+  mapping = cmp.mapping.preset.insert({
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping.confirm({ select = true })
-  }
+  })
 })
 
