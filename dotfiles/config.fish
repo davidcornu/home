64583ae7919bef4bbc24df1ea640d4db5aaf6e1d
David Cornu <me@davidcornu.com>
2025-01-01 23:15:11 -0500

    Configure completions for `jj`

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -44,6 +44,10 @@ if type -q direnv
   direnv hook fish | source
 end
 
+if type -q jj
+  jj util completion fish | source
+end
+
 if test -d "/Applications/1Password.app"
   set SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
 end
