0021046916039d977bce2c4e2f09ece87ac7a21a
David Cornu <me@davidcornu.com>
2025-06-05 14:19:30 -0400

    Add asdf setup

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -23,8 +23,19 @@ fish_add_path /opt/homebrew/bin
 fish_add_path "/nix/var/nix/profiles/default/bin"
 fish_add_path "$HOME/.nix-profile/bin"
 
-# Load asdf
-source ~/.asdf/asdf.fish
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 # Turn off greeting message
 set -U fish_greeting
